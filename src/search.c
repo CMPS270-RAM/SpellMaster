62e8b10d3caf7b4054a979f0aa5d25645c8840cf
JadJHaddad <jadjihadhaddad@gmail.com>
2023-10-05 23:56:26 +0300

    Started Sort

---
+++ src/search.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+int compareStrings ( char* w1, char* w2 ) {
+    
+    return 0;
+}
+
+char* binSearch ( char** spellList, char* spell ) {
+    char* foundWord;
+    return foundWord;
+}
+
+int main () {
+    char* w1 = "Word";
+    char* w2 = "Word";
+    
+}
