c4d8682e06088cce7f74f92a1f50b0a56c17f03c
Selena <swh11@mail.aub.edu>
2023-11-12 20:13:22 +0200

    fixed a bug in tally

--- src/Easy.c
+++ src/Easy.c
@@ -34,20 +34,24 @@ struct Tallier checkIfDefense(int length, char* read[], char* word) {
     struct Tallier content;
     content.c = 0;
     content.word = (char**)malloc(sizeof(char*));
+    content.word[0] = word;
 
-    char lastLetter = tolower(word[strlen(word) - 1]);
+    char firstLetter = tolower(word[strlen(word) -1]); // Get the first letter of the given word
 
     for (int j = 0; j < length; j++) {
-        char firstLetter = tolower(read[j][0]);
-        if ((firstLetter == lastLetter) ) {
+        char initialLetter = tolower(read[j][0]); // Get the first letter of the word in the read array
+
+        if (initialLetter == firstLetter) {
             content.c++;
-            content.word[0] = read[j];
-             printf("%s    %d\n", content.word[0], content.c);
         }
+        
     }
+    printf("%s   %d ", content.word[0],content.c);
 
     return content;
 }
+
+
 
 char* checkIfExists(char* read[], char* check[]) {
     int readLength = 0;
@@ -58,7 +62,7 @@ char* checkIfExists(char* read[], char* check[]) {
     char lastLetter = tolower(check[0][strlen(check[0]) - 1]);
 
     struct Node* list = NULL; // Initialize the list
-
+    int nodecount = 0;
     for (int j = 0; j < readLength; j++) {
         char firstLetter = tolower(read[j][0]);
 
@@ -68,6 +72,7 @@ char* checkIfExists(char* read[], char* check[]) {
             newNode->data = read[j];
             newNode->next = list;
             list = newNode;
+            nodecount++;
         }
     }
 
@@ -80,21 +85,43 @@ char* checkIfExists(char* read[], char* check[]) {
         count++;
         current = current->next;
     }
+    int min = tally[0].c;
+    for(int i = i; i<count; i++){
+      if(tally[i].c < min) min = tally[i].c;
+
+    }
    char * h = "hello";
+   int x =-1;
+  struct Node* current2 = list;
+  char* ret;
     if (count == 0) {
-      printf("from no words");
+      x = coinToss(2);
+      if( x == 0){
+        printf("from no defense x = 0");
+        return read[coinToss(readLength -1)];
+      }
+      else{
+       for(int i = 0; i < coinToss(nodecount); i++){
+        current2 = current2->next;
+       }
+       ret = current2->data;
+       
+       return ret;
+      }
+      printf("from no defense");
         // No matching words found
         return h;
     }
 
-    int x = coinToss(2);
+   
 
     if (x == 0) {
       printf("from x = 0");
-        return tally[0].word[0]; // Return the first matching string
+        return tally[min].word[0]; // Return the first matching string
     } else {
       printf("from else!");
-        int randomIndex = coinToss(count);
+        int randomIndex = coinToss(count - 1);
+        printf("%d",randomIndex);
         return tally[randomIndex].word[0]; // Return a random matching string
     }
 }
@@ -105,8 +132,8 @@ char* checkIfExists(char* read[], char* check[]) {
 
 
 int main() {
-    char* read[] = {"apple", "banana", "cherry", "date", "anana", "annana", NULL};
-    char* check[] = {"bob"};
+    char* read[] = {"apple", "elena", "erry", "eat", "ear", "annana", NULL};
+    char* check[] = {"ba"};
 
     printf("Matching strings:\n");
     char* result = checkIfExists(read, check);
