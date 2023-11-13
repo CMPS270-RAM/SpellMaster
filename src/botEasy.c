4ddca18131e816b52e54b1ce2357aadaccd5bda1
JadJHaddad <jadjihadhaddad@gmail.com>
2023-11-13 04:15:20 +0200

    adding specifications and test cases

--- src/botEasy.c
+++ src/botEasy.c
@@ -16,6 +16,26 @@ struct Tallier {
     char* word;
 };
 
+/*
+ * Specifications:
+ *
+ * pre: 
+ * read: should contain the spells that can be played during the game
+ * length: should be the length of the read list
+ * word: the word to be tallied for next possible plays
+ * usedwordList: a boolean list indicating if a word is used or not
+ * word must not be empty
+ * word should be part of the read list
+ * 
+ * post:
+ * returns a tallier struct with the word that was tallied and the number of possible plays after it
+ *
+ * Testing:
+ *
+ * case 1: test with a word with no possible next plays
+ * case 2: test with a word that has multiple next plays
+ *
+ */
 struct Tallier checkIfDefense(int length, char* read[], char* word, const bool* usedwordList) {
     struct Tallier content;
     content.c = 0;
@@ -40,7 +60,29 @@ struct Tallier checkIfDefense(int length, char* read[], char* word, const bool* 
     return content;
 }
 
-
+/*
+ * Specifications:
+ *
+ * pre:
+ * read: should contain the spells that can be played during the game
+ * readLength: should be the length of the read list
+ * check: the previously played word
+ * usedwordList: a boolean list indicating if a word is used or not
+ * turn: to determine if there was a previous spell or its the first player
+ *
+ * post:
+ * returns a word from the read list
+ * case 1: if a winning word exists (no next plays) play it
+ * case 2: if no winning word exists randomly plays the word with the highest tally ( hoping that the human doesn't see the win )
+ * case 3: or plays the word with the lowest tally ( hoping that the human doesn't find the word )
+ *
+ * Testing: 
+ * case 1: test that it is returning a valid word
+ * case 2: test that it is returning a word that has not been played before
+ * case 3: test that it is returning the win word if it exists
+ * case 4: test that it is returning the word with max or min possible plays if a win word doesn't exist
+ *
+ */
 char* botEasy (char* read[], char* check, const bool* usedwordList, int readLength, int turn) {
 
     char lastLetter;
