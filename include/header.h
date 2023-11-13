370daace7dcd4fc68dc1b4008865b80c9a9ce16b
JadJHaddad <jadjihadhaddad@gmail.com>
2023-11-13 05:23:53 +0200

    merge fixes

--- include/header.h
+++ include/header.h
@@ -9,6 +9,6 @@ int checkSpells(char **spellList, int spellCount, char *spell);
 char *botHard(char **spellList, const int spellCount, const bool *usedSpells, const char *previousSpell, int currentTurn);
 int coinToss (int n);
 bool checkIfSpellsRemain(char** spellList, int spellCount, bool* usedSpells, char* lastSpell);
-char* botMedium(char* spellList[],int spellListCount,const char* prevWord,const bool* usedwordList);
+char* botMedium(char* spellList[],int spellListCount,char* prevWord,bool* usedwordList);
 char* botEasy(char* read[], char* check, const bool* usedwordList, int readLength, int turn);
 void game(int numberOfPlayers, int botDifficulty);
