d9a2d345c2b92bc0110c4891dc309fc085bac86e
JadJHaddad <jadjihadhaddad@gmail.com>
2023-11-13 05:08:56 +0200

    fixing header file

--- include/header.h
+++ include/header.h
@@ -8,7 +8,7 @@ bool checkUsed(char **usedSpellList, int usedSpellCount, char *spell);
 int checkSpells(char **spellList, int spellCount, char *spell);
 int coinToss (int n);
 bool checkIfSpellsRemain(char** spellList, int spellCount, bool* usedSpells, char* lastSpell);
-char* botMedium(char* spellList[],int spellListCount,const char* prevWord,const bool* usedwordList);
+char* botMedium(char* spellList[],int spellListCount,char* prevWord,bool* usedwordList);
 char* botEasy(char* read[], char* check, const bool* usedwordList, int readLength, int turn);
 char* botHard();
 void game(int numberOfPlayers, int botDifficulty);
